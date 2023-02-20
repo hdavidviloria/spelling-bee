ade4400ce0c2b832c173e43d1af5b65e24248ee9
hdavidviloria <94162758+hdavidviloria@users.noreply.github.com>
2023-02-20 15:38:39 +0800

    Added changes
    Modified to make it work in Mac and to include multiple languages

--- bee.sh
+++ bee.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-# Copyright 2010 Tom White
+# Copyright 2023 David Viloria
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Based on code originally written by Tom White (https://github.com/tomwhite/) (c) 2011, licensed under the Apache License
+# Modificiations made by David Viloria involve mainly adopting this project on Mac, adding support for multiple langauges, etc
+# Such modifications are licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
@@ -16,27 +18,47 @@
 #
 
 function speak {
-  echo "$@" > /tmp/speak
-  espeak -v en-uk -s 120 -f /tmp/speak --stdout | aplay -q
+  local word=$1
+  local lang=$2
+
+  echo "$word" > /tmp/speak
+
+  if [ "$lang" = "chinese" ]; then
+    say -v Ting-Ting -r 150 -f /tmp/speak
+  elif [ "$lang" = "english" ]; then
+    say -v Samantha -r 200 -f /tmp/speak
+  elif [ "$lang" = "tagalog" ]; then
+    say -v Karen -r 200 -f /tmp/speak
+  else
+    echo "Unsupported language: $lang"
+  fi
 }
 
 while true; do
-  # read out a random word
-  word=$(sort -R words | head -1)
-  speak "How do you spell, $word"
+  # choose a random language
+  lang_idx=$((RANDOM%3))
+  if [ $lang_idx -eq 0 ]; then
+    lang="english"
+    word=$(sort -R words_en.txt | head -1)
+  elif [ $lang_idx -eq 1 ]; then
+    lang="chinese"
+    word=$(sort -R words_cn.txt | head -1)
+  else
+    lang="tagalog"
+    word=$(sort -R words_tl.txt | head -1)
+  fi
 
-  # wait for keypress
+  # speak the word in the selected language
+  speak "$word" "$lang"
+
+
   echo "Press a key for the answer"
   read -n 1 -s
 
-  # spell it out
-  letters=$(echo $word | tr '[A-Z]' '[a-z]' | sed 's/[a-z]/&, /g')
   echo $word
-  echo $letters
-  speak $letters
+ 
 
   # wait for keypress
   echo "Press a key for the next word"
   read -n 1 -s
-
 done
